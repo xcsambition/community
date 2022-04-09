8894ce37ac80e0b1e5da924da4db828798c390bc
xcs <1633697855@qq.com>
2022-04-10 01:34:53 +0800

    拦截器、实现登录状态信息、修好了bug

--- src/main/java/com/zfx/community/config/WebMvcConfig.java
+++ src/main/java/com/zfx/community/config/WebMvcConfig.java
@@ -19,7 +19,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor).
-                excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg").
-                addPathPatterns("/register","/login");
+                excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
     }
 }
