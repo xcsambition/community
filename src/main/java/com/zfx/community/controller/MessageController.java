09f8ce85b4f44f7846fab698ff39c163308b363f
xcs <1633697855@qq.com>
2022-09-22 17:22:35 +0800

    私信列表

---
+++ src/main/java/com/zfx/community/controller/MessageController.java
@@ -0,0 +1,105 @@
+package com.zfx.community.controller;
+
+import com.zfx.community.entity.Message;
+import com.zfx.community.entity.Page;
+import com.zfx.community.entity.User;
+import com.zfx.community.service.MessageService;
+import com.zfx.community.service.UserService;
+import com.zfx.community.utils.HostHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+public class MessageController {
+    @Resource
+    private MessageService messageService;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private HostHolder hostHolder;
+
+    @RequestMapping(value = "/letter/list", method = RequestMethod.GET)
+    public String getLetterList(Model model, Page page) {
+        User user = hostHolder.getUser();
+//        User user = userService.findUserById(111);
+
+        //分页
+        page.setLimit(5);
+        page.setPath("/letter/list");
+        page.setRows(messageService.findConversationCount(user.getId()));
+
+        //会话列表
+        List<Message> conversationList = messageService.findConversations(user.getId(), page.getOffset(), page.getLimit());
+        List<Map<String, Object>> conversations = new ArrayList<>();
+        if (conversationList != null) {
+            conversationList.forEach((message) -> {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("conversation", message);
+                map.put("letterCount", messageService.findLetterCount(message.getConversationId()));
+                map.put("unreadCount", messageService.findLetterUnreadCount(user.getId(), message.getConversationId()));
+                int targetId = user.getId() == message.getFromId() ? message.getToId() : message.getFromId();
+                map.put("target", userService.findUserById(targetId));
+                conversations.add(map);
+            });
+        }
+        HashMap<String, Object> res = new HashMap<>();
+//        res.put("conversations",conversations);
+//        res.put("letterUnreadCount",messageService.findLetterUnreadCount(user.getId(),null));
+        model.addAttribute("conversations", conversations);
+        model.addAttribute("letterUnreadCount", messageService.findLetterUnreadCount(user.getId(), null));
+
+        return "/site/letter";
+
+
+    }
+
+    @RequestMapping(path = "/letter/detail/{conversationId}", method = RequestMethod.GET)
+    public String getLetterDetail(@PathVariable String conversationId, Page page, Model model) {
+        page.setLimit(5);
+        page.setPath("/letter/detail/" + conversationId);
+        page.setRows(messageService.findLetterCount(conversationId));
+
+        //私信列表
+        List<Message> letterList = messageService.findLetters(conversationId, page.getOffset(), page.getLimit());
+
+        List<Map<String, Object>> letters = new ArrayList<>();
+        if (letterList != null) {
+            letterList.forEach((message) -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("letter", message);
+                map.put("fromUser", userService.findUserById(message.getFromId()));
+                letters.add(map);
+            });
+        }
+        model.addAttribute("letters", letters);
+
+        //私信目标
+        model.addAttribute("target", getLetterTarget(conversationId));
+
+        return "/site/letter-detail";
+
+
+    }
+
+    private User getLetterTarget(String conversationId) {
+        String[] ids = conversationId.split("_");
+        int d0 = Integer.parseInt(ids[0]);
+        int d1 = Integer.parseInt(ids[1]);
+
+        int targetId = hostHolder.getUser().getId() == d0 ? d1 : d0;
+
+        return userService.findUserById(targetId);
+    }
+}
