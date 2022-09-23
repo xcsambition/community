d58379b72e8dfba83a3ecaf6e7d24a60fdc066eb
xcs <1633697855@qq.com>
2022-09-23 19:57:51 +0800

    统一记录日志

---
+++ src/main/java/com/zfx/community/aspect/ServiceAspect.java
@@ -0,0 +1,41 @@
+package com.zfx.community.aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceAspect.class);
+
+    @Pointcut("execution(* com.zfx.community.service.*.*(..))")
+    public void pointcut() {}
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+//        用户[xxx],在[],访问了[com.zfx.community.service.xxx()].
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s]",ip,now,target));
+
+
+
+    }
+
+}
